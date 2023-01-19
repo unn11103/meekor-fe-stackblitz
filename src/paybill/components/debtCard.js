96d9e0f136ce8ae55b9348edb5e177d8b42f2466
unn <62011286@kmitl.ac.th>
2023-01-20 01:35:41 +0700

    [ADD] Added debtCard

---
+++ src/paybill/components/debtCard.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const DebtCard = () => {
+    return (
+    <div class="card lg:card-compact w-96 bg-base-100 shadow-xl">
+    <div class="card-body items-center text-center">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions">
+        <button class="btn btn-primary">Buy Now</button>
+        </div>
+    </div>
+    </div>            
+
+    );
+}
+
+export default DebtCard
--- src/paybill/personalBillPage.js
+++ src/paybill/personalBillPage.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
+import DebtCard from "./components/debtCard";
 
 const PersonalBillPage = () => {
-  return <div></div>;
+  return (
+  <div class="h-screen">
+    <h1>personalBillPage</h1>
+  <div class="h-96 carousel carousel-vertical rounded-box">
+    <div class="carousel-item h-full">
+      <DebtCard></DebtCard>  </div>  
+    </div>
+  </div>
+  );
 };
 export default PersonalBillPage;
